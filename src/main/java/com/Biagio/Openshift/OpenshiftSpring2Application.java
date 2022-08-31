534568ef80875b0c51390293c852d79c7dbc3695
BiagioEmulator <superdanielillo01@gmail.com>
2022-08-31 22:14:33 +0200

    First commit

---
+++ src/main/java/com/Biagio/Openshift/OpenshiftSpring2Application.java
@@ -0,0 +1,13 @@
+package com.Biagio.Openshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftSpring2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/Biagio/Openshift/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.Biagio.Openshift;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OpenshiftSpring2Application.class);
+	}
+
+}
